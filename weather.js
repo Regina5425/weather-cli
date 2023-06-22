0647933ea4d7c9da098fb2ed59b0576ce0f7cccd
Regina <reina5425@gmail.com>
2023-06-22 17:38:33 +0300

    dev to console

---
+++ weather.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.services.js";
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+	if (args.h) {
+		printHelp();
+	}
+	if(args.s) {
+		// сохранить город
+	}
+	if (args.t) {
+		// сохранить токен
+	}
+	// вывести погоду
+};
+
+initCLI();
